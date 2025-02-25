019df1ce582d18b35239f262156cb09fa98e3513
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-02-25 20:25:29 -0300

    Add files via upload

---
+++ exer_2.py
@@ -0,0 +1,18 @@
+"""exercicio 2
+autor: vinicius
+versao: 0.0.1"""
+
+# Alocação de memoria
+
+numero_2 = 2
+numero_3 = 3
+
+# Entrada de dados
+
+# Processamento de dados
+
+resultado = numero_2 + numero_3
+
+#Saída de dados
+
+print(resultado)
